3c3f6503805cf33b8e43093316e149061d897483
Boris TANOH <138991507+Boristanoh@users.noreply.github.com>
2025-12-29 17:48:45 +0000

    modif 3

---
+++ frontend/src/components/Contact/index.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+    <div>
+        <p>Formualaire de contact</p>
+          
+    </div>)
+}
+
+
+export default Contact
---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+    <div>
+        <p>Header de la page</p>
+          
+    </div>)
+}
+
+
+export default Header
